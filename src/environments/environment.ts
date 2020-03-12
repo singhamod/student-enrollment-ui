cbc8cf96ec0fae9deba6c5824a3a7bc492724bc0
gamodg <gamodg@gmail.com>
2020-03-13 00:44:34 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://ec2-13-211-174-118.ap-southeast-2.compute.amazonaws.com:8080'
 };
 
 /*
